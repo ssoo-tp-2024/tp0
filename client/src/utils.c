6f8f1e1f6408a2b451b2e5bd2185166496ef1d50
Nicolas Filippi Farmar <nfilippifarmar@frba.utn.edu.ar>
2024-04-04 04:01:27 +0000

    [CLIENT] Fix extra int

--- client/src/utils.c
+++ client/src/utils.c
@@ -30,9 +30,9 @@ int crear_conexion(char *ip, char *puerto)
 	err = getaddrinfo(ip, puerto, &hints, &server_info);
 
 	// Ahora vamos a crear el socket.
-	int socket_cliente = socket(server_info->ai_family,
-								server_info->ai_socktype,
-								server_info->ai_protocol);
+	socket_cliente = socket(server_info->ai_family,
+							server_info->ai_socktype,
+							server_info->ai_protocol);
 
 	// Ahora que tenemos el socket, vamos a conectarlo
 	err = connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
